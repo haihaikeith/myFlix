042d160a5a4fe9aea5d758583ed1a314c7e8dcd7
haimynameiskeith <68352456+haimynameiskeith@users.noreply.github.com>
2020-09-17 13:15:36 -0700

    directory changes
    moved files around
    added express

---
+++ public/index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+let topMovies = [
+  {
+    title: 'Grandma\'s Boy',
+    director: 'Nicholaus Goossenn'
+  },
+  { title: 'The Matrix Trilogy',
+    director: 'The Wachowski Sisters'
+  },
+  {
+    title: 'John Wick',
+    director: 'Chad Stahelski, David Leitch'
+  },
+  {
+    title: 'Starship Troopers',
+    director: 'Paul Verhoeven'
+  },
+  {
+    title: 'Goldeneye',
+    director: 'Martin Campbell'
+  },
+  {
+    title: 'The Witcher',
+    director: 'Lauren Schmidt'   
+  },
+  {
+    title: 'Scott Pilgrim vs. The World',
+    director: 'Edgar Wright'
+  },
+  {
+    title: 'Youth In Revolt',
+    director: ' Miguel Arteta'
+  },
+  {
+    title: 'Pan\'s Labyrinth',
+    director: 'Guillermo del Toro'
+  },
+  {
+    title: 'The Avengers',
+    director: '‎Joss Whedon'
+  }
+];
+
+// GET requests
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my film club!');
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('public/documentation.html', {root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
+
+// listenening for requests
+app.listen(8080, () => {
+    console.log('My movie app is hearing things on Port 8080.');
+});
