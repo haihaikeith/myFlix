3b4db4196ae9f2c99e0410978a7d7e5310061041
haimynameiskeith <68352456+haimynameiskeith@users.noreply.github.com>
2020-09-19 15:01:10 -0700

    Added movie details

--- public/index.js
+++ public/index.js
@@ -5,42 +5,92 @@ const app = express();
 let topMovies = [
   {
     title: 'Grandma\'s Boy',
-    director: 'Nicholaus Goossenn'
+    director: {
+      name:'Nicholaus Goossenn',
+      birthday: 'Born: August 18, 1978, Los Angeles, CA'
+    },
+    genre: 'Comedy',
+    description: 'A thirty-five-year-old video game tester has to move in with his grandma and her two old lady roommates.'
   },
   { title: 'The Matrix Trilogy',
-    director: 'The Wachowski Sisters'
+    director: {
+      name: 'Lana Wachowski, Lilly Wachowski',
+      birthday: 'Lana Wachowski - Born: June 21, 1965 (age 55 years), Chicago, IL, \nLily Wachowski - Born: August 18, 1978, Los Angeles, CA',
+    },
+    genre: 'Action',
+    description: 'A computer hacker learns from mysterious rebels about the true nature of his reality and his role in the war against its controllers. '
   },
   {
     title: 'John Wick',
-    director: 'Chad Stahelski, David Leitch'
+    director: {
+      name:'Chad Stahelski, David Leitch(uncredited)',
+      birthday: 'C.S. - 09.20.1968, D.L. - 11.16.1975',
+    },
+    genre: 'Action',
+    description: 'An ex-hit-man comes out of retirement to track down the gangsters that killed his dog and took everything from him. '
   },
   {
-    title: 'Starship Troopers',
-    director: 'Paul Verhoeven'
+    title: 'Cloud Atlas',
+    director: {
+      name:'Lana Wachowski, Lilly Wachowski, Tom Tykwer',
+      birthday: 'Lana Wachowski - Born: June 21, 1965 (age 55 years), Chicago, IL, \nLily Wachowski - Born: August 18, 1978, Los Angeles, CA, \nTom Tykwer - Born: May 23, 1965 (age 55 years), Wuppertal, Germany',
+    },
+    genre: 'Sci-Fi, Drama',
+    description: 'An exploration of how the actions of individual lives impact one another in the past, present and future, as one soul is shaped from a killer into a hero, and an act of kindness ripples across centuries to inspire a revolution. '
   },
   {
     title: 'Goldeneye',
-    director: 'Martin Campbell'
+    director: {
+      name: 'Martin Campbell',
+      birthday: 'Born: October 24, 1943 (age 76 years), Hastings, New Zealand'
+    },
+    genre: 'Action',
+    description: 'Years after a friend and fellow 00 agent is killed on a joint mission, a secret space based weapons program known as "GoldenEye" is stolen. James Bond sets out to stop a Russian crime syndicate from using the weapon.'
   },
   {
     title: 'The Witcher',
-    director: 'Lauren Schmidt'   
+    director: {
+      name: 'Lauren Schmidt',
+      birthday: 'Born: August 1, 1978 Westerville, Ohio'
+    },
+    genre: 'Fantasy',
+    description: 'Geralt of Rivia, a solitary monster hunter, struggles to find his place in a world where people often prove more wicked than beasts.'
   },
   {
     title: 'Scott Pilgrim vs. The World',
-    director: 'Edgar Wright'
+    director: {
+      name: 'Edgar Wright',
+      birthday: ' Born: April 18, 1974 in Poole, Dorset, England, UK '
+    },
+    genre: 'Adventure',
+    description:'Scott Pilgrim must defeat his new girlfriend\'s seven evil exes in order to win her heart. '
   },
   {
     title: 'Youth In Revolt',
-    director: ' Miguel Arteta'
+    director: {
+      name: 'Miguel Arteta',
+      birthday: 'Born: August 29, 1965, San Juan, Puerto Rico'
+    },
+    genre:  'Comedy',
+    description: 'While his trailer trash parents teeter on the edge of divorce, Nick Twisp sets his sights on dream girl Sheeni Saunders, hoping that she\'ll be the one to take away his virginity. '
   },
   {
     title: 'Pan\'s Labyrinth',
-    director: 'Guillermo del Toro'
+    director: {
+      name: 'Guillermo del Toro',
+      birthday: '10.09.1964',
+    }, 
+    genre: 'Fantasy, Drama',
+    description: ' In the Falangist Spain of 1944, the bookish young stepdaughter of a sadistic army officer escapes into an eerie but captivating fantasy world. '
   },
   {
     title: 'The Avengers',
-    director: '‎Joss Whedon'
+    director: {
+      name: '‎Joss Whedon',
+      birthday: '06.23.1964'
+    },
+    genre: 'Adventure, Sci-Fi',
+    description: 'Earth\'s mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.'
   }
 ];
 
