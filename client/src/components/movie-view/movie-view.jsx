4d323da9074c3a76fbf1ffbf3441fc310dab3a0c
haimynameiskeith <68352456+haimynameiskeith@users.noreply.github.com>
2020-11-11 13:53:59 -0800

    added back button

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -9,7 +9,7 @@ export class MovieView extends React.Component {
   }
 
   render() {
-    const { movie } = this.props;
+    const { movie, onClick } = this.props;
 
     if (!movie) return null;
 
@@ -33,7 +33,12 @@ export class MovieView extends React.Component {
           <span className="label">Director: </span>
           <span className="value">{movie.Director.Name}</span>
         </div>
+
+        <div><button onClick={() => onClick()}>Back</button>
+        </div>
       </div>
+
+
 
 
     );
